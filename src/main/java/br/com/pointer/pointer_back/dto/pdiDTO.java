189c855f248eadebb9de12c35c0843a1004551e4
MarcusMix <tradedomarcus@gmail.com>
2025-05-24 14:29:58 -0300

    fix: pdi dto fix (ok)

--- src/main/java/br/com/pointer/pointer_back/dto/pdiDTO.java
+++ src/main/java/br/com/pointer/pointer_back/dto/pdiDTO.java
@@ -1,32 +1,17 @@
 package br.com.pointer.pointer_back.dto;
 
 import br.com.pointer.pointer_back.enums.StatusPDI;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
-
 import java.time.LocalDate;
 
 @Data
 public class pdiDTO {
-
-    @NotBlank(message = "Título é obrigatório")
-    @Size(min = 2, max = 100, message = "Título deve ter entre 2 e 100 caracteres")
+    private Long id;
     private String titulo;
-
-    @NotBlank(message = "Descrição é obrigatória")
-    @Size(min = 2, max = 500, message = "Descrição deve ter entre 2 e 500 caracteres")
     private String descricao;
-
-    @NotNull(message = "Data de Início é obrigatória")
     private LocalDate dataInicio;
-
-    @NotNull(message = "Data Fim é obrigatória")
     private LocalDate dataFim;
-
-    @NotNull(message = "ID do Usuário é obrigatório")
     private Long idUsuario;
-
+    private Long destinatario;
     private StatusPDI status;
-} 
+}

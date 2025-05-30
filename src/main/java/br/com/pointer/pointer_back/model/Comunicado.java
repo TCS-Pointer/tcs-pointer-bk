9a161bbc910429721de3f5b81428f71dfc8fc3d7
mateus00000 <mateus.c000@outlook.com>
2025-05-30 19:37:29 -0300

    feat: Criação do Crud de Comunicados

---
+++ src/main/java/br/com/pointer/pointer_back/model/Comunicado.java
@@ -0,0 +1,42 @@
+package br.com.pointer.pointer_back.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "comunicado")
+public class Comunicado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private String setor;
+
+    @Column(nullable = false)
+    private String cargo;
+
+    @Column(name = "dt_publicacao", nullable = false)
+    private LocalDateTime dataPublicacao;
+
+    @PrePersist
+    protected void onCreate() {
+        dataPublicacao = LocalDateTime.now();
+    }
+}

304ebe331e081083de9b35cc387151904397bbf1
alceu12 <55637357+alceu12@users.noreply.github.com>
2025-05-30 20:15:38 -0300

    Feat: new functions for user

--- src/main/java/br/com/pointer/pointer_back/repository/UsuarioRepository.java
+++ src/main/java/br/com/pointer/pointer_back/repository/UsuarioRepository.java
@@ -9,22 +9,22 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>, JpaSpecificationExecutor<Usuario> {
-    Optional<Usuario> findByEmail(String email);
 
-    boolean existsByEmail(String email);
+    Optional<Usuario> findByKeycloakId(String keycloakId);
+
+    boolean existsByKeycloakId(String keycloakId);
 
     @Query("SELECT u FROM Usuario u WHERE " +
            "(:setor IS NULL OR u.setor = :setor) AND " +
-           "(:cargo IS NULL OR u.cargo = :cargo) AND " +
-           "(:status IS NULL OR u.status = :status)")
+           "(:cargo IS NULL OR u.cargo = :cargo)")
     Page<Usuario> findByFilters(
         @Param("setor") String setor,
         @Param("cargo") String cargo,
-        @Param("status") String status,
         Pageable pageable
     );
 }

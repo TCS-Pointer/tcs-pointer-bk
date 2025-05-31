c6ada5ce6e9e589ab8b7400fad9db984756fda9a
MarcusMix <tradedomarcus@gmail.com>
2025-05-31 16:19:27 -0300

    config: mvc config (for pdi)

---
+++ src/main/java/br/com/pointer/pointer_back/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package br.com.pointer.pointer_back.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.core.Ordered;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.setOrder(Ordered.LOWEST_PRECEDENCE);
+    }
+}

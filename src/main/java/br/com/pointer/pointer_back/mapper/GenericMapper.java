effad466edd3fc685e459891d6a41cdb72b40fc6
alceu12 <55637357+alceu12@users.noreply.github.com>
2025-06-04 20:03:19 -0300

    refactor: remove generecicMapper

--- src/main/java/br/com/pointer/pointer_back/mapper/GenericMapper.java
+++
@@ -1,7 +0,0 @@
-package br.com.pointer.pointer_back.mapper;
-
-public interface GenericMapper<DTO, ENTITY, RESPONSE> {
-    ENTITY toEntity(DTO dto);
-    RESPONSE toResponseDTO(ENTITY entity);
-    void updateEntityFromDTO(DTO dto, ENTITY entity);
-}

cd3d235c936e0670f5cad491bba76fe0763e0c9f
arthur <arthurolov@gmail.com>
2025-06-23 21:31:43 -0300

    fix: rota de listagem de usuarios ajustada

--- src/main/java/br/com/pointer/pointer_back/controller/FeedbackController.java
+++ src/main/java/br/com/pointer/pointer_back/controller/FeedbackController.java
@@ -1,14 +1,20 @@
 package br.com.pointer.pointer_back.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import br.com.pointer.pointer_back.ApiResponse;
 import br.com.pointer.pointer_back.dto.FeedbackDTO;
 import br.com.pointer.pointer_back.dto.FeedbackResponseDTO;
+import br.com.pointer.pointer_back.dto.FeedbackStatsResponseDTO;
 import br.com.pointer.pointer_back.service.FeedbackService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/feedback")
@@ -19,22 +25,36 @@ public class FeedbackController {
 
     @GetMapping("/recebidos")
     public ApiResponse<Page<FeedbackResponseDTO>> listarFeedbacksRecebidos(
-            @RequestParam Long userId,
+            @RequestParam String keycloakId,
             @RequestParam(required = false) String keyword,
             @RequestParam(defaultValue = "0") int page) {
-        return feedbackService.listarFeedbacksRecebidos(userId, keyword, page);
+        return feedbackService.listarFeedbacksRecebidos(keycloakId, keyword, page);
     }
 
     @GetMapping("/enviados")
     public ApiResponse<Page<FeedbackResponseDTO>> listarFeedbacksEnviados(
-            @RequestParam Long userId,
+            @RequestParam String keycloakId,
             @RequestParam(required = false) String keyword,
             @RequestParam(defaultValue = "0") int page) {
-        return feedbackService.listarFeedbacksEnviados(userId, keyword, page);
+        return feedbackService.listarFeedbacksEnviados(keycloakId, keyword, page);
     }
-// usando o feedback dto para retornar o feedback criado.
+
+    @GetMapping("/all")
+    @PreAuthorize("hasRole('admin')")
+    public ApiResponse<Page<FeedbackResponseDTO>> listarTodosFeedbacks(
+            @RequestParam(required = false) String keyword,
+            @RequestParam(defaultValue = "0") int page) {
+        return feedbackService.listarTodosFeedbacks(keyword, page);
+    }
+
     @PostMapping("/novo")
-    public ApiResponse<FeedbackResponseDTO> criarFeedback(@RequestBody FeedbackDTO feedbackDTO) {
+    public ApiResponse<Void> criarFeedback(@RequestBody FeedbackDTO feedbackDTO) {
         return feedbackService.criarFeedback(feedbackDTO);
+    }
+
+    @GetMapping("/stats")
+    @PreAuthorize("hasRole('admin')")
+    public ApiResponse<FeedbackStatsResponseDTO> getFeedbackStats() {
+        return feedbackService.getFeedbackStats();
     }
 } 

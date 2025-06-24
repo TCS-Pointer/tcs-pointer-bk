2bdf1e0c7cc9f9ed2a227a10916576bdd51c9cca
arthur <arthurolov@gmail.com>
2025-06-23 21:43:30 -0300

    feat: criado DTO para tratamento do campo de competencias do forms

---
+++ src/main/java/br/com/pointer/pointer_back/dto/CompetenciasDTO.java
@@ -0,0 +1,11 @@
+package br.com.pointer.pointer_back.dto;
+
+import lombok.Data;
+
+@Data
+public class CompetenciasDTO {
+    private Integer avComunicacao;
+    private Integer avProdutividade;
+    private Integer resolucaoDeProblemas;
+    private Integer trabalhoEmEquipe;
+}

d21732cad19676fcb1358c00b004032194f3663a
arthur <arthurolov@gmail.com>
2025-06-23 22:10:22 -0300

    feat: criado DTOs para transferencias dos dados do feedback completo

---
+++ src/main/java/br/com/pointer/pointer_back/dto/DestinatarioDTO.java
@@ -0,0 +1,10 @@
+package br.com.pointer.pointer_back.dto;
+
+import lombok.Data;
+
+@Data
+public class DestinatarioDTO {
+    private Long usuarioId;
+    private String nome;
+    private String cargo;
+} 
